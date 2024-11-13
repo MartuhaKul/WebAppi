4adfbfcc01c8ad975d095df5ccad53272ecc8bbf
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-13 15:25:42 +0200

    1

--- obj/Debug/net8.0/WebAppi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAppi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316781f9a7f2ef01d1695dd76208c5b420ca47c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1022d3b99134a12d2adb2c770efabf84cfe13d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
