00464f3c5fccc966cfebd4eb1bbf46f0d84dd403
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-13 03:46:34 +0200

    іконка працює

--- obj/Debug/net8.0/WebAppi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAppi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9db561d1b5f1bb98549619cdefd585757bb2481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0add1e244a51143e9892c40766fddd31e11e3d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
