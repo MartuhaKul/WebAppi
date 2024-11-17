114ec224124f9da805dc4717b301f2ca31694731
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-17 14:58:31 +0200

    працює кнопка бай

--- obj/Debug/net8.0/WebAppi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAppi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb813fdbfeae1eded90c490941519ac326d86d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dfd99c1bbeffa9dc677930ce6bb8b9f7b4d3d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
